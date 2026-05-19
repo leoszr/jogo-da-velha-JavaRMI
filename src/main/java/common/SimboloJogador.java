be697b91096e9498b482612867469aca1e65971f
leonardo <leo.srodrigues01@gmail.com>
2026-05-19 14:24:41 -0300

    feat: implementa TASK-002 enums do dominio

---
+++ src/main/java/common/SimboloJogador.java
@@ -0,0 +1,6 @@
+package common;
+
+public enum SimboloJogador {
+    X,
+    O
+}
---
+++ src/main/java/common/StatusPartida.java
@@ -0,0 +1,9 @@
+package common;
+
+public enum StatusPartida {
+    AGUARDANDO_JOGADORES,
+    EM_ANDAMENTO,
+    X_VENCEU,
+    O_VENCEU,
+    EMPATE
+}
